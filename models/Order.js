d5991a1d7dbaee37b029775806c379e6d5effbd5
omar <202027471@std.sci.cu.edu.eg>
2022-12-24 02:54:32 +0200

    modify order schema

--- models/Order.js
+++ models/Order.js
@@ -2,29 +2,69 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-    userId: {
-        type: String,
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        require:true,
+        ref:"users",
     },
-    items: [{
-        productId: {
-            type: String,
-        },
-        name: String,
-        quantity: {
-            type: Number,
-            required: true,
-            min: [1, 'Quantity can not be less then 1.']
-        },
-        price: Number
-    }],
-    bill: {
-        type: Number,
-        required: true
+    orderItems:[
+        {
+            name:{type:String,required:true},
+            qty:{type:Number,required:true},
+            image:{type:String,required:true},
+            price:{type:Number,required:true},
+            car:{
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                ref:"cars",
+            },
+        }
+    ],
+    shippingAddress:{
+        address:{type:String,required:true},
+        city:{type:String,required:true},
+        country:{type:String,required:true},
     },
-    date_added: {
-        type: Date,
-        default: Date.now
-    }
-})
+    paymentMethod:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Credit"
+    },
+    paymentResult:{
+        id:{type:String},
+        status:{type:String},
+        update_time:{type:String},
+        email_address:{type:String},
+    },
+    shippingPrice:{
+        type:Number,
+        require:true,
+        default:0.0,
+    },
+    totalPrice:{
+        type:Number,
+        require:true,
+        default:0.0,            
+    },
+    isPaid:{
+        type:Boolean,
+        require:true,
+        default:false,
+    },
+    paidAt:{
+        type:Date,
+    },
+    idDelivered:{
+        type:Boolean,
+        require:true,
+        default:false,
+    },
+    deliveredAt:{
+        type:Date,
+    },
+},{
+    timestamps: true
+}
+)
 
 module.exports = Order = mongoose.model('order',OrderSchema);
