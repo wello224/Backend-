d4cb06da255d61606920bce5e899dcbd44a2a164
mokhtar <mohamedmokhtarm3@gmail.com>
2022-12-25 06:58:14 +0200

    Re-modify Car Schema to link it with other schemas

--- models/Car.js
+++ models/Car.js
@@ -1,28 +1,47 @@
 const mongoose = require('mongoose');
+const deepPopulate = require('mongoose-deep-populate')(mongoose);
+const findVisible = require('./findVisible');
+
 const Schema = mongoose.Schema;
 
 const CarSchema = new Schema(
-  {
-    name: {type: String, required: true},
-    model: {type: String, required: true},
-    image: {type: String, required: true},
-    payPerDay: {type: Number, required: true},
-    fuelType: {type: String, required: true},
-    capacity: {type: Number, required: true},
-    stock: {type:Number,required: true,min: [1, 'Quantity can not be less then 1.'],
-    default: 1},
-    bookedTimeSlots: [
+    {
+        name: { type: String, required: true },
+        model: { type: String, required: true },
+        image: { type: String, required: true },
+        payPerDay: { type: Number, required: true },
+        fuelType: { type: String, required: true },
+        capacity: { type: Number, required: true },
+        stock: {
+            type: Number, required: true, min: [1, 'Quantity can not be less then 1.'],
+            default: 1
+        },
+        bookedTimeSlots: [
+            {
+                from: { type: String, required: true },
+                to: { type: String, required: true }
+            }
+        ],
+        isVisible:
         {
-            from: {type: String, required: true},
-            to: {type: String, required: true}
+            type: Boolean, default: true
         }
-    ],
-   
-}, {
+
+    }, {
     timestamps: true
-// 
+    // 
 }
 );
-const Car = mongoose.model('car', CarSchema);
+
+
+const population =[]
+
+CarSchema.pre('find', findVisible(population));
+CarSchema.pre('findOne', findVisible(population));
+CarSchema.pre('findOneAndUpdate', findVisible());
+CarSchema.pre('count', findVisible());
+CarSchema.pre('countDocuments', findVisible());
+
+const Car = mongoose.model('Car', CarSchema);
 module.exports = Car;
 
